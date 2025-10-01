31c6eb1279ad80c07ee24ee7a329fb1f2aa1186e
Nikita Bilavare <95965469+Nikita17128275@users.noreply.github.com>
2025-10-01 16:32:24 +0530

    Update Reverse_String.c

--- Reverse_String.c
+++ Reverse_String.c
@@ -6,12 +6,14 @@ int main() {
     char str[200];
     printf("Enter String : ");
     fgets(str,sizeof(str),stdin);
+    str[strcspn(str, "\n")] = '\0';
     char rev[200];
     int j=0;
     for(int i=strlen(str)-1;i>=0;i--){
         rev[j]=str[i];
         j++;
     }
+    rev[j]='\0';
     printf("rev strr: %s",rev);
     return 0;
 }
