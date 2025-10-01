5b9115574d5234427cd20eb82b34a5a63945b9a1
Nikita Bilavare <95965469+Nikita17128275@users.noreply.github.com>
2025-10-01 11:38:08 +0530

    Update Prime_Number.c

--- Prime_Number.c
+++ Prime_Number.c
@@ -6,11 +6,15 @@ int main() {
     printf("Enter number: ");
     scanf("%d",&a);
     bool prime=true;
-    for(int i=2;i<a;i++){
+    if(a==0||a==1){
+        printf("Not prime");
+    }else {
+    for(int i=2;i<a/2;i++){
        if (a % i == 0){
           prime = false;
           break;
       }
+    }
     }
     if(prime) printf("prime");
     else printf("Not prime");
